2212eeabe9c661d28d6c451f95d88cc046eee3f9
Casenrique <carloshenriquesouza.eng@gmail.com>
2022-09-12 15:56:24 -0300

    Adicionando exercícios após feedback

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,20 +1,26 @@
-let desejaComerMaisCoxinha = prompt(`Você gostaria de comer uma coxinha?
-Digite S se deseja comer uma coxinha
-Digite N se não deseja comer.
-`).toUpperCase()
-let conta = 0
+let desejaComerMaisCoxinha
+let conta = 0.00
 
-if(desejaComerMaisCoxinha === `N` || desejaComerMaisCoxinha === `S`){
-    while(desejaComerMaisCoxinha !== `N`){
-        conta = conta + 2.50;
+
+while(desejaComerMaisCoxinha !== `N`){
         desejaComerMaisCoxinha = prompt(`Você gostaria de comer mais uma coxinha?
 Digite S se deseja comer mais uma coxinha
 Digite N se não deseja comer mais coxinhas
         `).toUpperCase()
+        if(desejaComerMaisCoxinha === "S"){
+        conta = conta + 2.50;}
     }
-    console.log(`O valor total da sua conta é ${conta} reais
-    `)
-}else{
-    console.log(`Digite S ou N, por favor!`)
-    alert(`Digite S ou N, por favor!`)
-}
+console.log(`O valor total da sua conta é ${conta} reais
+`)
+
+// if(desejaComerMaisCoxinha === `N` || desejaComerMaisCoxinha === `S`){
+//     while(desejaComerMaisCoxinha !== `N`){
+//         if(desejaComerMaisCoxinha === `S`){
+//         conta = conta + 2.50;
+//         desejaComerMaisCoxinha = prompt(`Você gostaria de comer mais uma coxinha?
+// Digite S se deseja comer mais uma coxinha
+// Digite N se não deseja comer mais coxinhas
+//         `).toUpperCase()
+//     }
+// }}
+// console.log(`O valor total da sua conta é ${conta} reais`)
